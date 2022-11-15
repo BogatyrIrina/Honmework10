6fde87d30f4e3163d5cc509916b59951c20e63a9
BogatyrIrina <kochereva_irina@mail.ru>
2022-11-15 23:41:03 +0300

    Задание 1 - 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Задание 1
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Имя сотрудника - " + fullName);
+
+        //Задание 2
+        System.out.println("Задание 2");
+
+        String upperFullName = fullName.toUpperCase();
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upperFullName);
+
+        //Задание 3
+        System.out.println("Задание 3");
+
+        String fullName2 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName2.replace("ё", "е"));
     }
 }
